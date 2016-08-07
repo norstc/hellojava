ad3fe709cd2c435c33a8d3fffc598c1446cc9c38
amble2002 <amble2002@sina.com>
2016-08-07 18:17:36 +0800

    add file read and write
    Todo: find the stock close, try to use Swing html parser

--- hello/src/webutil/GetWebContent.java
+++ hello/src/webutil/GetWebContent.java
@@ -1,6 +1,9 @@
 package webutil;
 
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.io.*;
 
 
@@ -32,9 +35,14 @@ public class GetWebContent {
 		this.result = result;
 	}
 	
-	//this is real method to find the content from net
+	/**
+	 * this is real method to find the content from net
+	 */
+
+	
 	public String findIt(){
-		String ret = null;
+		String dividendInfo = null; //the result will send back
+		dividendInfo =code + " "; // start with stock code 
 		boolean start= false;
 		boolean end = false;
 		try {
@@ -44,17 +52,37 @@ public class GetWebContent {
 			
 			String inputLine;
 			while((inputLine = in.readLine()) != null){
+				
+				//get the stock name
+				if(inputLine.contains("<title>")){
+					dividendInfo = dividendInfo + getStockName(inputLine);
+					//System.out.println(dividendInfo);
+				}
+				
+				//get the stock close price
+				if(inputLine.contains("stockclose")){
+					dividendInfo += getStockClose(inputLine);
+				}
+				//get the stock dividend
+				List<String> dividendLines = new ArrayList<String>();
 				if(inputLine.contains("<!-- 分红送配 begin -->")){
 					start = true;
-					ret = getDivident(inputLine);
+					
 				}
 				if(inputLine.contains("<!-- 分红送配 end -->")){
 					start = false;
 					end = true;
 				}
 				if(start && !end){
-					System.out.println(inputLine);
+					dividendLines.add(inputLine);
+					
 				}
+				if(!start && end){
+					end = false;
+					dividendInfo += getDivident(dividendLines);
+				}
+				
+				
 			}
 			
 			in.close();
@@ -67,14 +95,43 @@ public class GetWebContent {
 			e.printStackTrace();
 		}
 		
-		
-		setResult(  ret);
+		//System.out.println(dividendInfo);
+		setResult(dividendInfo);
 		return getResult();
 	}
 
-	private String getDivident(String inputLine) {
-		// TODO Auto-generated method stub
-		return inputLine;
+	/**
+	 * 获取收盘价
+	 * Swing有html parser
+	 * Jsoup也可以用
+	 * 待分析
+	 * @param inputLine
+	 * @return
+	 */
+	private String getStockClose(String inputLine) {
+		// TODO  <p id="stockclose" class="stock"><span id="stock_quoteinfo_xj" class="red">15.73</span><span class="img"></span></p>
+		String ret = 15.73+ "  ";
+		
+		return ret;
+	}
+
+	private String getStockName(String inputLine) {
+		// TODO <title>浦发银行(600000)_融资分红_证券之星</title>
+		int x = inputLine.indexOf("<title>");
+		int y = inputLine.indexOf("(");
+		String ret = inputLine.substring(x+7, y)+ "  "; //need 2 space, 1 space will be omitted
+		//System.out.println(ret);
+		return ret;
+		
+	}
+
+	private String getDivident(List<String> dividendLines) {
+		// TODO find the dividend from the dividend Lines
+		for(String line:dividendLines){
+			System.out.println(line);
+		}
+		String ret = "we 'll make it happen" ;
+		return ret;
 	}
 
 	private String getRank(String inputLine) {
