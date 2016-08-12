f2091532ecbc7c2b9e4f901e5987cecbcbc3d420
norstc <norstc@gmail.com>
2016-08-12 14:37:13 +0800

    add bike and mixed
    Bike: demo for inheritance 
    Mixed: test

---
+++ hello/src/Bike/Bike.java
@@ -0,0 +1,17 @@
+package Bike;
+
+public class Bike {
+	private double length;
+	
+	public Bike(){
+		setLength(10);
+	}
+
+	public double getLength() {
+		return length;
+	}
+
+	public void setLength(double length) {
+		this.length = length;
+	}
+}
