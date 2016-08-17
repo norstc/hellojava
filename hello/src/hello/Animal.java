cedacd31dcb22c2fcf6fcb8ad89391bd12b4794c
norstc <norstc@gmail.com>
2016-08-17 10:31:24 +0800

    add Hippo and HippoTest
    to show construct chaining

--- hello/src/hello/Animal.java
+++ hello/src/hello/Animal.java
@@ -2,6 +2,9 @@ package hello;
 
 public class Animal {
 
+	public Animal(){
+		System.out.println("Making an animal");
+	}
 	public void eat(){
 		System.out.println("eat some");
 	}
---
+++ hello/src/hello/Hippo.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class Hippo extends Animal{
+
+	public Hippo(){
+		super();
+		System.out.println("Making a Hippo...");
+		
+	}
+}
