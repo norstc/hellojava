add5567b733e417d8a1df53dc7dfd7abff6ce327
amble2002 <amble2002@sina.com>
2016-09-01 21:25:33 +0800

    add InterfaceDemo
    super class and interface has the same name variable

---
+++ hello/src/hello/InterfaceDemo.java
@@ -0,0 +1,16 @@
+package hello;
+
+
+class fat{
+	static int x = 1;
+}
+interface face{
+	int x = 0;
+}
+public class InterfaceDemo extends fat implements face{
+	
+	public static void main(String[] args){
+		System.out.println("super x is " + fat.x);
+		System.out.println("interface x is " + face.x);
+	}
+}
